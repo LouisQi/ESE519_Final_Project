b6ee10440b1ea45f6f1ba4a410f923e980baedf6
Xueyang Qi <xueyangq@seas.upenn.edu>
2023-12-08 06:11:55 -0500

    Done!

---
+++ ESE519_Final_Project-main/ESE519FinalProject/Debug/FreeRTOS/Source/list.d
@@ -0,0 +1,64 @@
+FreeRTOS/Source/list.d FreeRTOS/Source/list.o: ../FreeRTOS/Source/list.c \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../FreeRTOS/Source/include/FreeRTOS.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../FreeRTOS/Source/include/../../FreeRTOSConfig.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ C:\Users\wuxue\Desktop\ese519\7.0\Packs\Atmel\ATmega_DFP\2.1.506\include/avr/iom328p.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../FreeRTOS/Source/include/projdefs.h \
+ ../FreeRTOS/Source/include/portable.h \
+ ../FreeRTOS/Source/include/deprecated_definitions.h \
+ ../FreeRTOS/Source/portable/GCC/ATMega328/portmacro.h \
+ ../FreeRTOS/Source/include/mpu_wrappers.h \
+ ../FreeRTOS/Source/include/list.h
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../FreeRTOS/Source/include/FreeRTOS.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../FreeRTOS/Source/include/../../FreeRTOSConfig.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+C:\Users\wuxue\Desktop\ese519\7.0\Packs\Atmel\ATmega_DFP\2.1.506\include/avr/iom328p.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../FreeRTOS/Source/include/projdefs.h:
+
+../FreeRTOS/Source/include/portable.h:
+
+../FreeRTOS/Source/include/deprecated_definitions.h:
+
+../FreeRTOS/Source/portable/GCC/ATMega328/portmacro.h:
+
+../FreeRTOS/Source/include/mpu_wrappers.h:
+
+../FreeRTOS/Source/include/list.h:
---
+++ ESE519_Final_Project-main/ESE519FinalProject/Debug/lib/hx711.d
@@ -0,0 +1,60 @@
+lib/hx711.d lib/hx711.o: ../lib/hx711.c ../lib/hx711.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Users\wuxue\Desktop\ese519\7.0\Packs\Atmel\ATmega_DFP\2.1.506\include/avr/iom328p.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\atomic.h \
+ c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+
+../lib/hx711.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Users\wuxue\Desktop\ese519\7.0\Packs\Atmel\ATmega_DFP\2.1.506\include/avr/iom328p.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\atomic.h:
+
+c:\users\wuxue\desktop\ese519\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
